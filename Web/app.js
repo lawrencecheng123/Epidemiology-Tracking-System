838294cc91f8ffca8e84cd191a89bc1ff666f6fc
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-04-09 20:19:49 -0700

    fix

---
+++ Web/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.controller('suggestionController', function($scope) {
+  $scope.todos = [
+      {text:"todo1"},
+      {text:"todo2"},
+      {text:"todo3"}
+    ]
+  });
--- Web/suggestions.js
+++
@@ -1,9 +0,0 @@
-var MoriApp = angular.module('MoriApp', []);
-
-MoriApp.controller('suggestionController', function($scope) {
-  $scope.todos = [
-      {text:"todo1"},
-      {text:"todo2"},
-      {text:"todo3"}
-    ]
-  });
